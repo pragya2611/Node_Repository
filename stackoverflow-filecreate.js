75246485c617070ba9770bd6df4d0e591f1871cc
pragya2611 <pragyat2611@gmail.com>
2019-09-01 10:43:28 +0530

    stackoverflow-filecreate

--- stackoverflow-filecreate.js
+++ stackoverflow-filecreate.js
@@ -1,15 +1,11 @@
 var fs = require('fs'); 
   
-// Use readFileSync() method 
-  
-// Store the result (return value) of this 
-// method in a variable named readMe 
+
 for(let i=0 ; i < 4 ; i++) {
+    //create json data and stringify to write 
     var readMe = JSON.stringify({"data" : i});
-     
+     //write file sync data
     fs.writeFileSync('writeMe' + i + '.json', readMe, "utf8");
 }
 
-   
-// Store the content and read from 
-// readMe.txt to a file WriteMe.txt 
+
